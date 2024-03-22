c1158f4338729d072324f80c62f979850fdebd9c
mario <hah3m>
2024-03-22 00:24:46 -0500

    v1 84

--- src/MyList.hpp
+++ src/MyList.hpp
@@ -5,6 +5,10 @@
 
 // These are freebies, touch only if attempting doubly-linked list
 // Tests may depend on these working!
+// linked list only no double
+#ifndef MYLIST_HPP
+#define MYLIST_HPP
+#include "MyList.h"
 template <typename T> ListNode<T> *MyList<T>::begin() { return head; }
 
 template <typename T> MyList<T>::MyList() {
@@ -39,29 +43,113 @@ template <typename T> void MyList<T>::pop_front() {
 // PLEASE READ MyList.h carefully for details on each!
 // Check out unit_tests/ and mem_tests/ for grading details
 
-template <typename T> MyList<T>::~MyList() {}
+// deallocate
+template <typename T> MyList<T>::~MyList() {
+  clear();
+  delete head;
+}
 
+// check from pa002
 template <typename T> MyList<T> &MyList<T>::operator=(const MyList<T> &rhs) {
-
-  // Finish Me!
+  // check ,clear and then copy from rhs
+  clear();
+  ListNode<T> *currentPos = head;
+  ListNode<T> *dataPos = rhs.head;
+  while (dataPos->next != nullptr) {
+    insert(currentPos, dataPos->data);
+    currentPos = currentPos->next;
+    dataPos = dataPos->next;
+  }
 
   return *this;
 }
+// see previous copy
+template <typename T> MyList<T>::MyList(const MyList<T> &rhs) {
+  head = new ListNode<T>;
+  head->next = nullptr;
+  *this = rhs;
+}
 
-template <typename T> MyList<T>::MyList(const MyList<T> &rhs) {}
+// asign value-push
+template <typename T> void MyList<T>::assign(int count, const T &value) {
+  this->clear();
+  // loop
+  for (int i = 0; i < count; i++) {
+    this->push_front(value);
+  }
+}
 
-template <typename T> void MyList<T>::assign(int count, const T &value) {}
+// remove
+template <typename T> void MyList<T>::clear() {
+  ListNode<T> *temp;
+  temp = head->next;
+  while (temp != nullptr) {
+    delete head;
+    head = temp;
+    temp = head->next;
+  }
+  num_elements = 0;
+}
+// see example
+template <typename T>
+void MyList<T>::insert(LinkedListIterator<T> iter, const T &value) {
+  ListNode<T> *temp = new ListNode<T>;
+  temp->data = iter->data;
+  temp->next = iter->next;
+  iter->data = value;
+  iter->next = temp;
 
-template <typename T> void MyList<T>::clear() {}
+  num_elements++;
+}
+
+template <typename T> void MyList<T>::erase(LinkedListIterator<T> iter) {
+  ListNode<T> *temp = iter->next; // delete
+  iter->data = temp->data;
+  iter->next = temp->next; // next
+  delete temp;
+  num_elements--;
+}
+// reverse order previous,current and next
+template <typename T> void MyList<T>::reverse() {
+  ListNode<T> *previous = nullptr;
+  ListNode<T> *current = head;
+  ListNode<T> *next = head->next;
+
+  while (current != nullptr) {
+    next = current->next;
+    current->next = previous;
+    previous = current;
+    current = next;
+  }
+  head->next = previous;
+  head = previous->next;
+  previous->next = nullptr;
+}
+
+template <typename T> LinkedListIterator<T> MyList<T>::find(const T &value) {
+  // use example
+  ListNode<T> *temp = head;
+  while (temp->next != nullptr) {
+    if (value == temp->data)
+      return temp;
+    temp = temp->next;
+  }
+  return nullptr;
+}
+
+// ceck
 
 template <typename T>
-void MyList<T>::insert(LinkedListIterator<T> iter, const T &value) {}
+void MyList<T>::splice_after(ListNode<T> *splice_point, MyList<T> &source) {
 
-template <typename T> void MyList<T>::erase(LinkedListIterator<T> iter) {}
+  // get size -insert to list
+  // use pop front insertertion
+  int source_size = source.size();
+  for (int i = 0; i < source_size; i++) {
+    this->insert(LinkedListIterator<T>(splice_point), source.front());
+    splice_point = splice_point->next;
+    source.pop_front();
+  }
+}
 
-template <typename T> void MyList<T>::reverse() {}
-
-template <typename T> LinkedListIterator<T> MyList<T>::find(const T &value) {}
-
-template <typename T>
-void MyList<T>::splice_after(ListNode<T> *splice_point, MyList<T> &source) {}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,22 @@
         Check the README for specifications!
 */
 
+// store,sum,newlist
 #include "MyList.h"
 
-
-void HashFunction(const int& key);
+void HashFunction(const int &key);
 void ApplyHashFunction(MyList<int> keys);
 
 int main() {
   MyList<int> l;
 
-  string line;
+  int line;
   while (std::cin >> line) {
+    l.push_front(line);
 
     // Finish Me!
   }
+  ApplyHashFunction(l);
 
   return 0;
 }
@@ -25,8 +27,27 @@ int main() {
 
 // Takes in an integer (hash key) and prints the corresponding hash-code
 // (see README)
-void HashFunction(const int &key) {}
+void HashFunction(const int &key) { cout << key << endl; }
+
+int HashSum(const int &key) {
+  int num = key;
+  int sum = 0;
+  while (num != 0) {
+    sum += num % 10;
+    num /= 10;
+  }
+  return sum;
+}
 
 // Takes a list of hash keys
 // Prints the hash codes for every element
-void ApplyHashFunction(MyList<int> keys) {}
+void ApplyHashFunction(MyList<int> keys) {
+  ListNode<int> *current = keys.begin();
+  while (current != nullptr) {
+    int hash_sum = HashSum(current->data);
+    HashFunction(hash_sum);
+    // current->data = HashSum(current->data);
+    // HashFunction(current->data);
+    current = current->next;
+  }
+}
